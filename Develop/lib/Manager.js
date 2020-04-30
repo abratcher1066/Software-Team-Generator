02208a84b74fb2c19e35f982d8d04911413cc4cf
tanksareforcowards <37027181+tanksareforcowards@users.noreply.github.com>
2020-04-29 20:36:03 -0400

    cleanup

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,13 +1,8 @@
     // TODO: Write code to define and export the Manager class. 
-    
-    
     // HINT: This class should inherit from Employee.
     // dont have this done yet but i know how to inherit
-
 //     In addition to `Employee`'s properties and methods, `Manager` will also have:
-
 //   * officeNumber
-
 //   * getRole() // Overridden to return 'Manager'
 
 const Employee = require("./Employee")
@@ -15,36 +10,16 @@ const Employee = require("./Employee")
 class Manager extends Employee {
     constructor(name, email, id, officeNumber) {
         // call object parent stuff with super
-      super(name, email, id);
+    super(name, email, id);
       // need ot define new object property AND make it retrievable
-    //   this.prop1 = prop1;
-    //   this.prop2 = prop2;
     this.officeNumber = officeNumber;
-    // this.role = role;
     }
-    // and if anyone asks what this is
-    // ie.  * getRole() // Returns 'Employee'
     getRole() {
-      // return something...
-    return 'Manager';
+        return 'Manager';
     }
     getOfficeNumber() {
-        return this.officeNumber
-
+        return this.officeNumbe
     }
-
-  
-    setProp1() {
-      // do something...
-    }
-      
   }
-  
-
-
-class Manager extends Employee {
-    constructor (name, email, id)
-
-}
 
 module.exports = Manager;
