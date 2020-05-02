998b7d6b1ae3a9e58ff896a66d57bbb3f80776e6
tanksareforcowards <37027181+tanksareforcowards@users.noreply.github.com>
2020-05-01 23:25:33 -0400

    Fixed up validate functions, final cleanup before v2.0

--- Develop/app.js
+++ Develop/app.js
@@ -8,43 +8,42 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// This file will generate the final HTML. You don't need to touch this at all!
 const render = require("./lib/htmlRenderer");
 
-// This will be an array of all team member objects created
 const teamMembers = [];
 
-// This will be an array of the id values created for each object so there are no duplicates
 const idArray = [];
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// // char check function
-//      validate: answer => {
-//        validChars(answer)
-//        }
-// adding validate function to save space
+// Validation Functions
+
+// validChars --- Serves as validator for most answers
 function validChars(answer) {
-    if(answer !== "") {
-      return true;
-    }
+  if(answer == "") {
     return "Please enter at least one character."
   }
-
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// adding id checker function to make sure each team member has a unique id
-function validId(answer){
-  if (idArray.includes(answer)){
-      return "Please choose a unique ID # 0-9."
+    return true;
   }
-  return true;
+
+// validId --- serves as validator for IDs
+function validId(answer){
+  var isnum = /^\d$/.test(answer);
+  if(isnum === false){
+    return "Please enter a single number, 0-9."
+  }
+  if(answer == "") {
+    return "Please enter at least one character."
+  }
+  if (idArray.includes(answer)){
+    return "Please choose a unique ID # 0-9."
+  }
+  if(isNaN(answer)){
+    return "Please enter a number."
+  }
+    return true;
 }
-// ID check function
-//      validate: answer => {
-//        validId(answer)
-//        }
+
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// STUDENT: This function generates all the questions for creating the manager. You need to add more to this.
 function createManager(){
   console.log("Please build your team");
   inquirer.prompt([
@@ -65,7 +64,7 @@ function createManager(){
       type: "input",
       name: "managerId",
       message: "What is your manager's ID number?",
-      validate: validChars
+      validate: validId
     },
     {
       type: "input",
@@ -91,8 +90,6 @@ function createTeam() {
         choices: ['manager', 'intern', 'engineer', 'finished']
     }
   ]).then(userChoice => {
-    // STUDENT: Based on which choice they make, call the correct function to ask more questions.
-    // If no choice is made, then go to the rendering function.
       switch (userChoice.addnewteammember) {
         case 'manager':
           createManager();
@@ -117,30 +114,18 @@ function createEngineer() {
       name: "engineerName",
       message: "What is your engineer's name?",
       validate: validChars
-      // validate: answer => {
-      //   validChars(answer)
-      // }
     },
     {
       type: "input",
       name: "engineerEmail",
       message: "What is your engineer's email?",
       validate: validChars
-      // validate: answer => {
-      //   validChars(answer)
-      // }
     },
     {
       type: "input",
       name: "engineerId",
       message: "What is your engineers's ID number?",
-      validate: validChars
-      // validate: answer => {
-      //   validChars(answer)
-      // },
-      // validate: answer => {
-      //   validId(answer)
-      // }
+      validate: validId
     },
     {
       type: "input",
@@ -150,13 +135,6 @@ function createEngineer() {
     }
 
   ]).then(answers => {
-    // STUDENT: Make sure the id supplied is unique,
-    // gonna need a function for that.........................................
-    
-    // then take the data supplied and 
-    // instantiate the Engineer constructor.
-    // Add the new object to the team member array
-    // Pass control back to the createTeam() function
       var engineer = new Engineer(answers.engineerName, answers.engineerEmail, answers.engineerId, answers.github)
       teamMembers.push(engineer);
       idArray.push(answers.engineerId);
@@ -164,7 +142,6 @@ function createEngineer() {
   });
 }
 
-// STUDENT: Now create a function for creating an Intern using the code above as an example
 function createIntern() {
   inquirer.prompt([
     {
@@ -172,30 +149,18 @@ function createIntern() {
       name: "internName",
       message: "What is your intern's name?",
       validate: validChars
-      // validate: answer => {
-      //   validChars(answer)
-      // }
     },
     {
       type: "input",
       name: "internEmail",
       message: "What is your intern's email?",
       validate: validChars
-      // validate: answer => {
-      //   validChars(answer)
-      // }
     },
     {
       type: "input",
       name: "internId",
       message: "What is your intern's ID number?",
-      validate: validChars
-      // validate: answer => {
-      //   validChars(answer)
-      // },
-      // validate: answer => {
-      //   validId(answer)
-      // }
+      validate: validId
     },
     {
       type: "input",
@@ -205,13 +170,6 @@ function createIntern() {
     }
 
   ]).then(answers => {
-    // STUDENT: Make sure the id supplied is unique,
-    // gonna need a function for that.........................................
-    
-    // then take the data supplied and 
-    // instantiate the Engineer constructor.
-    // Add the new object to the team member array
-    // Pass control back to the createTeam() function
       var intern = new Intern(answers.internName, answers.internEmail, answers.internId, answers.internSchool)
       teamMembers.push(intern);
       idArray.push(answers.internId);
@@ -219,54 +177,13 @@ function createIntern() {
   });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// STUDENT: This function will call the render function required near the top (line 12), 
-// and pass INTO it the teamMembers area; from there, write the HTML returned back to a file 
-// in a directory called output.
 function renderHtmlPage(){
   let html = render(teamMembers)
   fs.writeFileSync(outputPath, html)
-
 }
 
-// This is our starter function.
-// Note that we use separate functions for different questions in inquirer to 
-// help keep code organized.
 function startMenu() {
-
-  // Here we start things off by calling the createManager function
   createManager()
-
 }
-
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 
 startMenu();
